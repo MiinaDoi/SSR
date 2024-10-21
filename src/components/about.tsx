c82d54c2d7a6b068018a2338020d0e5283f5f651
Sugar1201 <140928.miina@gmail.com>
2024-10-21 17:01:05 +0900

    Create Portfolio Page

---
+++ src/components/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './about.css';
+
+const AboutSection: React.FC = () => {
+  return (
+    <div className="about-section">
+      <h2>About me</h2>
+      <p>
+      Hello, I'm Sugar. I am undergraduate student at University in Japan. I am currently studying frontend development and seeking internships at IT companies both domestically and internationally. I have individual and team development experience with a technical stack that includes React.js, Next.js, and React Native.
+      </p>
+    </div>
+  );
+};
+
+export default AboutSection;
